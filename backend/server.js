600232a8f253e944047f45b8ed082a929f00a217
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-07 19:54:42 +0500

    fix: completely remove kill-port

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const mongoose  = require('mongoose');
 const cors      = require('cors');
 const morgan    = require('morgan');
 const path      = require('path');
-
 const { Server } = require('socket.io');
 require('dotenv').config();
 
