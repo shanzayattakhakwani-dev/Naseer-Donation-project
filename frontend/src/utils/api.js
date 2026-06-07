369d2c82642e147759285003edae34bb09f362e6
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-07 23:20:08 +0500

    fix: dynamic API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL 
+    ? `${process.env.REACT_APP_API_URL}/api` 
+    : '/api' 
+});
 
 // Attach JWT token to every request
 api.interceptors.request.use((config) => {
